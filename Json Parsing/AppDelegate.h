a24eaf136d9feba18f45d4cfa7d50f0487f0d87e
Manas <mdresswalla@gmail.com>
2016-07-19 17:20:40 +0530

    Initial Commit

---
+++ Json Parsing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Json Parsing
+//
+//  Created by Apple on 19/07/16.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Json Parsing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Json Parsing
+//
+//  Created by Apple on 19/07/16.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
